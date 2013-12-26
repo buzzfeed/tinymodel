6261ecea5b03b9276d88e70053dacf91c899eae4
Jeff <jeff.revesz@buzzfeed.com>
2013-12-26 16:05:00 -0500

    bumped version to 0.1.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except ImportError:
 
 setup(
     name='tinymodel',
-    version='0.1.1',
+    version='0.1.2',
     author='Jeff Revesz',
     author_email='jeff.revesz@buzzfeed.com',
     packages=find_packages(),
